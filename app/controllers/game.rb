48081f65b9ee069572afff5be04ffd73ed49a6ac
jamesascarter <jamesascarter@gmail.com>
2014-12-20 18:43:38 +0000

    amended multiplayer views page and route in game controller

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -10,7 +10,12 @@ post '/gamepage' do
   @name =session[:name]
   PLAYER.weapon_choice = params[:weapon_choice]
   @computer_choice = GAME.computer_choice
+  erb :gamepage
 end
+
+get '/multiplayer' do
+	erb :multiplayer
+	end
 
 get '/waiting_page' do
   erb :waiting_page
